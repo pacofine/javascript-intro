8e1d25c6a48c04d919aaf35c066f5cd83eeda9d5
sebastian <srodriguez@spartaglobal.com>
2019-01-11 11:45:45 +0000

    first commit

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+// alert("hello")
+console.log("HI IN CONSOLE");
+var myNumber = 90;
+console.log(myNumber);
+
+var n1 = 10;
+var n2 = 20;
+
+console.log(n1, n2);
+
+console.log(n1 + n2);
+
+console.log("number", n1,   "plus number", n2, "equals", n1+n2);
